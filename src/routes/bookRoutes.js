8196497a7a81945190b482036ddc3c7bbc28f8d1
Alexy Cruz <ACruz@uvltd.com>
2024-09-15 19:34:56 -0600

    minor change on routes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 
-// Define the search route first
+// Define specific routes first
 router.get('/search', bookController.searchBooks);
+router.get('/categories', bookController.getAllCategories);
 
-// Other routes
+// Then define the more general routes
 router.get('/', bookController.getAllBooks);
 router.post('/', bookController.createBook);
 router.get('/:id', bookController.getBookById);
@@ -13,6 +14,5 @@ router.put('/:id', bookController.updateBook);
 router.delete('/:id', bookController.deleteBook);
 router.post('/:id/copy', bookController.copyBook);
 router.post('/:id/decrease-copy', bookController.decreaseCopy);
-router.get('/categories', bookController.getAllCategories);
 
 module.exports = router;
